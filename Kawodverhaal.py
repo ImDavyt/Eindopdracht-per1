78b113afc61b3842b27ba661b9f49c32848f9b8b
Davy <32709@ma-web.nl>
2021-11-05 12:27:12 +0100

    Eindopdracht

---
+++ Kawodverhaal.py
@@ -0,0 +1,183 @@
+import time 
+import os 
+
+
+def rus():
+    print("A: Hij pakt de Молоко")
+    print("B: Hij pakt de йогурт")
+
+    scriptinput = input()
+    if scriptinput == "A" or scriptinput == "a":
+        os.system("cls")
+        print ("Geen idee wat Молоко is, maar ik neem het  Denkt Kawod. Het lijkt op melk, dus dan zou het vast wel goed zitten. Hij rekent af, en neemt een slok. Mmmm, dit had ik even nodig. Hij had gelukkig de goede melk. Stel je voor dat het Yogurt zou zijn.")
+        print ("")
+        time.sleep(4)
+        print("Kawod Heeft het goed in rusland, Hij vermaakt zich, en is gelukkig weg van de oorlog. Hij hoopt dat het goed gaat met zijn familie, en natuurlijk Taco! “Hoe zou Taco het nu hebben?” Vraagt hij zich af. Maar Kawod heeft het goed, en leeft lang en gelukkig")
+        time.sleep(4)
+        print("")
+        print("Einde")
+
+        scriptinput = input()
+    if scriptinput == "B" or scriptinput == "b":
+        os.system("cls")
+        print ("Geen idee wat йогурт is, maar we nemen het  Denkt Kawod Het lijkt op melk, dus dan zou het vast wel goed zitten. Hij rekent af, en neemt een slok. Gatver Bahhh. Kawod spuugt het gelijk uit. Dit is geen melk, het is Yogurt! Kawod rent terug de winkel in, en haalt Молоко wat dus melk is. Zodra hij het heeft gekocht neemt hij gelijk een slok. Mmmm, dit had ik even nodig. Hij had gelukkig nu wel de goede melk.")
+        print ("")
+        time.sleep(5)
+        print("Kawod Heeft het goed in rusland, Hij vermaakt zich, en is gelukkig weg van de oorlog. Hij hoopt dat het goed gaat met zijn familie, en natuurlijk Taco! “Hoe zou Taco het nu hebben?” Vraagt hij zich af. Maar Kawod heeft het goed, en leeft lang en gelukkig")
+        time.sleep(5)
+        print("")
+        print("Einde")
+
+
+
+def begin():
+    os.system("cls")
+    print("")
+    print("Wij moesten een hele stressvolle keuze maken. Namelijk naar welk land wij moesten gaan. Wij hadden de keuze uit twee verschillende landen: Rusland, een heel mooi, groot land. Waar je een goede baas hebt. Of Nederland, Waar de welvaart goed is, en je vrije keuzes hebt. Er is ook een vlucht die over 5 minuten vertrekt naar ibiza Duss…")
+    time.sleep(7)
+    print("A: Kawod gaat naar Rusland, en Taco naar Nederland")
+    print("B: Kawod gaat naar Nederland, en Taco naar Rusland")
+    print("C: Ze gaan samen naar Ibiza")
+    scriptinput = input()
+
+    if scriptinput == "A" or scriptinput == "a":
+        os.system("cls")
+        print ("Goed, naar Rusland dan maar? Zegt Kawod. Taco reageert: Dan ga ik naar Nederland. dan moeten we wel snel een vliegticket regelen. Dus dat doen ze maar. Kawod neemt het vliegtuig richting Rusland. Het was een vlucht van 3 uur lang, maar hij komt er wel. Na een lange vlucht, Is Kawod in Rusland aangekomen. Het is erg koud, maar wel mooi. Terwijl hij uitstapt, ziet hij ineens politie rennen. “Ach joh, het zal wel” denkt Kawod. Kawod vraagt aan een taxichauffeur, of hij een leuke slaapplek weet. De taxi chauffeur wijst ze naar het stadje Kazan. Dat was 2 uur met de taxi. Kawod keek de taxi chauffeur aan, en stapte toch maar in. Na een lange rit achter de rug te hebben. Is hij heel erg moe, en boekt het hotel. Dan ziet hij dat er een deluxe kamer is, en een normale kamer…")
+        print ("")
+        script = True
+
+        print("")
+        print("A: Kawod neemt de deluxe kamer")
+        print("B: Kawod neemt de normale kamer")
+
+        scriptinput = input()
+        if scriptinput == "A" or scriptinput == "a":
+            os.system("cls")
+            print ("Het was echt een prachtige nacht. Kawod heeft heerlijk geslapen, en is lekker fris en fruitig. Op naar een goede dag! Hij loopt het hotel weer uit, en ziet de prachtige zon op zn gezicht. Het is nog steeds koud, maar dat maakt hem op dit moment niks uit. Kawod moet naar de winkel om even wat melk te halen, want he, melk is goed voor elk! Hij loopt een supermarkt binnen, en zoekt voor melk. Hij ziet iets staan wat erop lijkt, maar hij weet het niet zeker…")
+            print ("")
+            rus()
+        
+        scriptinput = input()
+        if scriptinput == "B" or scriptinput == "b":
+            os.system("cls")
+            print ("Het was de slechtste nacht in tijden. Kawod heeft niet heel lekker geslapen. Het bed kraakte, en lag niet lekker. En het stonk ook nog op de kamer. Hij loopt de kamer uit, recht de kou in. “Brrr waarom is het zo koud?” Vraagt kawod zich af. Hij moet naar de winkel om even wat melk te halen, want he, melk is goed voor elk! Hij loopt een supermarkt binnen, en zoekt voor melk. Hij ziet iets staan wat erop lijkt, maar hij weet het niet zeker…")
+            print ("")
+            rus()
+
+    elif scriptinput == "B" or scriptinput == "b":
+        os.system("cls")
+        print ("Goed naar Nederland dan maar? Zegt kawod. Taco reageert: dan ga ik naar Rusland! laten we maar snel een vliegticket halen zodat we er zo snel mogelijk zijn! De broers nemen afscheid van elkaar, en zwaaien elkaar uit. “Ik ga je missen” schreeuwt kawod nog. Maar Taco hoort hem niet. Eenmaal aangekomen op de boarding kijkt Kawod naar het vliegtuig. Het vliegtuig is heel erg groot. Hij stapt in, het is een vlucht van 11 uur en 15 minuten dus Kawod gaat lekker slapen. De vlucht vloog voorbij en voor dat hij het wist was hij in Nederland. Hij stapt uit het vliegtuig, en rook de lekkere tulpen.")
+        print ("")
+        script = True
+
+        print("Kawod wilt graag naar een lekker hotel in de buurt gaan, om te kunnen overnachten, alleen hij heeft geen geld meer. Hij loopt schiphol uit. Als hij naar rechts gaat, kan hij lopend bij het hotel komen. Dit duurt alleen wel 45 minuten. Hij kijkt ook naar links, en daar staan veel fietsen. Als er een fiets mist, maakt dat toch niet uit? Duss…")
+        print("A: Je steelt een fiets")
+        print("B: Je gaat lopend naar het hotel")
+
+        scriptinput = input()
+        if scriptinput == "A" or scriptinput == "a":
+            os.system("cls")
+            print ("Goed.. het moet maar. Kawod kijkt om zich heen of er niet politie in de buurt is. Hij ziet niemand. Vervolgens zoekt hij een mooie fiets uit…")
+            print ("A: Een mooie roze meisjesfiets")
+            print("B: Een zwarte race fiets... maar heeft wel een slappe band")
+
+            scriptinput = input()
+        if scriptinput == "A" or scriptinput == "a":
+            os.system("cls")
+            print ("Hij ziet hem, een mooie roze meisjesfiets. Hij kruipt ernaartoe, en snipt het slot door. Niemand heeft het gezien, en hij gaat er snel vandoor. Op dat moment komt het meisje net terug, en begint te schreeuwen: Houd de dief!! Kawod trapt sneller dan ooit, maar het is niet genoeg. Hij ziet het stopteken van de politie al en hij wist, dit is foute boel…")
+            time.sleep(5)
+            print ("")
+            print("Einde")
+
+            scriptinput = input()
+        if scriptinput == "B" or scriptinput == "b":
+            os.system("cls")
+            print ("Hij ziet hem, een goede race fiets. Hij negeert de zachte band en hij gaat ervoor. Kawod kruipt naar de fiets toe, en knipt het slot los. Op dat moment pakt hij de fiets op, en wilt weg rijden. Op dat moment komt er net politie langs. En kijkt Kawod raar aan. Ze zetten de auto aan de kant, en geven Kawod het seintje dat hij moet stoppen. Kawod stopt en is erg zenuwachtig. Hij word ondervraagd door de agenten. Het lijkt een rustige sfeer te hebben. Tot hij ineens in de boeien werd geslagen. Kawod vraagt: Waarom wordt ik meegenomen? De agenten reageerde met: Die fiets stond al op gestolen, niet heel slim. De rechter bepaald nu nog de straf van Kawod, die hij later te horen gaat krijgen.")
+            time.sleep(5)
+            print ("")
+            print ("Einde")
+
+        scriptinput = input()
+        if scriptinput == "B" or scriptinput == "b":
+            os.system("cls")
+            print ("Zuchhtt… Dan maar lopen. Kawod gaat lopen. Na 50 minuten en wat oponthoud is hij eindelijk aangekomen. Hij ploft op zijn bed, en valt direct in slaap..")
+            print ("")
+            time.sleep(5)
+            print("Kawod word echt lekker wakker. Nog nooit heeft hij zo lekker geslapen. Kawod blijft nog even in zijn bed liggen, denkent aan hoe zijn familie het zou hebben. Hoe erg en naar hun het hebben. Hij denkt: Hoe zou het zijn met Taco..? Zou het goed zitten?")
+            time.sleep(5)
+            print("")
+            print("Een paar jaar later heeft Kawod een geweldig leven opgebouwd. Hij heeft een huis, succesvolle baan, en nu ook kinderen! Hij heeft twee jongetjes. En hij heeft ze Kawod en Taco JR genoemd, omdat ze door zoveel zware tijden hebben gestreden… - Einde")
+
+    elif scriptinput == "C" or scriptinput == "c":
+        os.system("cls")
+        print ("Goed, rennen!!! Kawod en Taco rennen naar de balie voor tickets. Maar het was al telaas, de tickets waren uitverkocht… Kawod smeekt de receptioniste nog voor de tickets. En ineens vind ze toch nog twee tickets voor het vliegtuig! Kawod en Taco kunnen hun ogen niet geloven! Ze nemen de tickets aan, betalen, en rennen nog naar het vliegtuig. Maar helaas, ze zagen door het raam het vliegtuig opstijgen. Taco begint te huilen, hij zegt: mmmaar… wat nu? Toen werd er ineens op Kawod zn schouder geklopt. Het is de politie. Hij zegt dat het hier niet veilig is en we met hem mee moeten. Kawod en Taco kijken elkaar aan, en gaan toch maar mee. Eenmaal aangekomen op het politiebureau, worden ze naar beneden geleid. Kawod vertrouwt dit niet en vraagt: Waar gaat dit heen als ik vragen mag? Ineens worden ze in de boeien geslagen, en in de cel gezet. De politieagent bleek helemaal geen agent te zijn. Maar een martelaar. Hij schreeuwt: Jullie zijn van mij!! Jullie krijgen niks te eten, en doen wat ik zeg!! Taco praat hem nog tegen, maar het werkt niet… Het was al te laat. Kawod kijkt Taco aan en zegt: Dit was het dan. Hier eindigt ons verhaal. Later die week werd de martelaar opgepakt, maar bleken Taco en Kawod al dood - Einde")
+        print ("")
+        time.sleep(10)
+        print("Klik op een knop om door te gaan")
+        script = True
+
+print ("Hello, mijn naam is Kawod Zwakofzki")
+print ("Wat is jou naam?")
+
+time.sleep(1)
+username = input("Enter username:")
+print ("Hallo: " + username)
+print ("Dit verhaal gaat over mijn vlucht tijd en mijn tijd in irak. Je kan keuzes maken, dus denk rustig na, en maak de goede keuzes!")
+    
+time.sleep(4)
+print ("")
+print ("Hallo, wij zijn Kawod En Taco Zwakofzki. Wij zijn broers. Kawod is 40, en Taco is 39.Wij komen oorspronkelijk uit Irak, maar daar moesten wij helaas weg. Wij moesten onze ouders en andere familieleden helaas achterlaten. Zodat wij het nog konden overleven, en ons leven voort kunnen zetten. Dit is de kant van Kawod")
+
+time.sleep(8)
+print ("")
+print("Het was 20 maart toen het begon, de oorlog tegen terrorisme wij hadden er niks mee te maken en hoopte dat de oorlog ons zo min mogelijk zou beïnvloeden en dat wij in ons land konden blijven maar tevergeefs de bombardementen waren gestart en wij hebben veel familie en vrienden verloren toen moesten wij echt weg. maar wel vervoer gaan we nemen zegt Taco tegen Kawod we kunnen lopen maar dat is wel heel gevaarlijk, we kunnen de taxi nemen maar dat is wel heel duur en we kunnen fietsen alleen dan zijn we wel heel moe als we daar aankomen.")
+time.sleep(3)
+print("A: Kawod en Taco gaan lopen naar het vliegveld")
+print("B: Kawod en Taco gaan met de taxi naar het vliegveld")
+print("C: Kawod en Taco gaan met de fiets naar het vliegveld")
+
+
+scriptinput = input()
+if scriptinput == "B" or scriptinput == "b":
+    os.system("cls")
+    print ("")
+    print ("Kawod en Taco gaan met de taxi naar het vliegveld, het is een aardige bestuurder die goed weet wat die doet. je hebt het met hem over zijn familie en de situatie in het land, en het is erg gezellig uiteindelijk zijn jullie halverwege en zijn jullie uitgepraat het is heel even stil. maar uiteindelijk zet de bestuurder een muziekje aan. je bent nu op het vliegveld de rit was erg duur en jullie zijn nu een deel van jullie budget kwijt.")
+    print("")
+    print("Klik op enter om door te gaan")
+    input()
+    script = True
+
+
+
+if scriptinput == "A" or scriptinput == "a":
+    begin()
+
+
+if scriptinput == "B" or scriptinput == "b":
+    os.system("cls")
+    print ("")
+    print ("Kawod en Taco gaan met de taxi naar het vliegveld, het is een aardige bestuurder die goed weet wat die doet. je hebt het met hem over zijn familie en de situatie in het land, en het is erg gezellig uiteindelijk zijn jullie halverwege en zijn jullie uitgepraat het is heel even stil. maar uiteindelijk zet de bestuurder een muziekje aan. je bent nu op het vliegveld de rit was erg duur en jullie zijn nu een deel van jullie budget kwijt.")
+    print("")
+    time.sleep(4)
+    print("Klik op enter om door te gaan")
+    input()
+    begin()
+    script = True
+    
+    scriptinput = input()
+if scriptinput == "C" or scriptinput == "c":
+    os.system("cls")
+    print ("")
+    print ("Kawod en Taco gaan fietsen naar het vliegveld, jullie conditie is niet erg goed maar het kan ermee door. jullie beginnen met fietsen en praten. jullie hebben het over dat jullie de familie erg gaan missen, en hopen dat het goed met hun komt. halverwege krijgt Taco een lekke band maar gelukkig weet Kawod hoe die je band plakt dus jullie kunnen door.") 
+    print("")
+    time.sleep(4)
+    print("Klik op enter om door te gaan")
+    input()
+    begin()
+
+
+
+
+
+
+
